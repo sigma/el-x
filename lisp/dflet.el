5928b4c13762b346d9a9e4211ff0a0dae24f7544
Yann Hodique <yann.hodique@gmail.com>
2013-10-05 21:57:01 -0700

    new dflet implementation, based on cl-letf

--- lisp/dflet.el
+++ lisp/dflet.el
@@ -1,7 +1,6 @@
 ;;; dflet.el --- dynamically-scoped flet
 
 ;; Copyright (C) 2012  Yann Hodique
-;; Copyright (C) 1993, 2001-2012  Free Software Foundation, Inc.
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords: lisp
@@ -33,13 +32,6 @@
   (require 'macroexp)
   (require 'subr-compat))
 
-;;; silence byte-compiler
-(eval-when-compile
-  (if (version< emacs-version "24.3")
-      ;; sure it doesn't exist, but it won't be called anyway...
-      (autoload 'cl--compiling-file "cl")
-    (declare-function cl--compiling-file "cl" t t)))
-
 (if (version< emacs-version "24.3")
     ;; before that version, flet was not marked as obsolete, so use it
     (defalias 'dflet 'flet)
@@ -49,34 +41,15 @@
     "Make temporary overriding function definitions.
 This is an analogue of a dynamically scoped `let' that operates on the function
 cell of FUNCs rather than their value cell.
-If you want the Common-Lisp style of `flet', you should use `cl-flet'.
-The FORMs are evaluated with the specified function definitions in place,
-then the definitions are undone (the FUNCs go back to their previous
-definitions, or lack thereof).
 
 \(fn ((FUNC ARGLIST BODY...) ...) FORM...)"
     (declare (indent 1) (debug cl-flet))
-    `(letf ,(mapcar
-             (lambda (x)
-               (if (or (and (fboundp (car x))
-                            (eq (car-safe (symbol-function (car x))) 'macro))
-                       (cdr (assq (car x) macroexpand-all-environment)))
-                   (error "Use `labels', not `dflet', to rebind macro names"))
-               (let ((func `(cl-function
-                             (lambda ,(cadr x)
-                               (cl-block ,(car x) ,@(cddr x))))))
-                 (when (cl--compiling-file)
-                   ;; Bug#411.  It would be nice to fix this.
-                   (and (get (car x) 'byte-compile)
-                        (error "Byte-compiling a redefinition of `%s' \
-will not work - use `labels' instead" (symbol-name (car x))))
-                   ;; FIXME This affects the rest of the file, when it
-                   ;; should be restricted to the flet body.
-                   (and (boundp 'byte-compile-function-environment)
-                        (push (cons (car x) (eval func))
-                              byte-compile-function-environment)))
-                 (list `(symbol-function ',(car x)) func)))
-             bindings)
+    `(cl-letf ,(mapcar
+                (lambda (x)
+                  (list
+                   (list 'symbol-function (list 'quote (car x)))
+                   (cons 'lambda (cons (cadr x) (cddr x)))))
+                bindings)
        ,@body)))
 
 ;;;###autoload
